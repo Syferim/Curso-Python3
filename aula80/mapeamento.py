ed9728ed65352ea87c6f95e4a6a698f80ea82b12
syferim <syfer.1616@gmail.com>
2022-04-05 19:34:44 -0300

    commit

---
+++ aula80/mapeamento.py
@@ -0,0 +1,11 @@
+from dados import produtos, pessoas, lista
+
+# nova_lista = map(lambda x: x * 2, lista)
+
+nova_lista = [x *2 for x in lista]
+print(lista)
+print(list(nova_lista))
+
+
+for produto in produtos:
+    print(produto)
---
+++ dados.py
@@ -0,0 +1,27 @@
+produtos = [
+    {'nome': 'p1', 'preço': 13},
+    {'nome': 'p1', 'preço': 55.55},
+    {'nome': 'p1', 'preço': 5.59},
+    {'nome': 'p1', 'preço': 22},
+    {'nome': 'p1', 'preço': 81.23},
+    {'nome': 'p1', 'preço': 5.7},
+    {'nome': 'p1', 'preço': 10.90},
+    {'nome': 'p1', 'preço': 89.82},
+    {'nome': 'p1', 'preço': 12},
+    {'nome': 'p1', 'preço': 2.90},
+]
+
+pessoas = [
+    {'nome': 'Luiz', 'idade': 12},
+    {'nome': 'Eduarda', 'idade': 43},
+    {'nome': 'Joana', 'idade': 65},
+    {'nome': 'Lucas', 'idade': 14},
+    {'nome': 'Felipe', 'idade': 17},
+    {'nome': 'Maria', 'idade': 19},
+    {'nome': 'Julia', 'idade': 25},
+    {'nome': 'Diniz', 'idade': 32},
+    {'nome': 'Max', 'idade': 23},
+    {'nome': 'Eduardo', 'idade': 59},
+]
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
