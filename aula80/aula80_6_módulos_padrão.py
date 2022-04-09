168cb7ca7b17c7d9388e7677ee6f35488cd8e5ff
syferim <syfer.1616@gmail.com>
2022-04-08 21:12:44 -0300

    commit

---
+++ aula80/aula80_6_módulos_padrão.py
@@ -0,0 +1,15 @@
+# módulos padrão do python
+# módulos são arquivos .py (que contém código phyhon) e servem para expandir
+# as funcionalidades padrão da
+# (linguagem)veja todos os módulos padrão do python em:
+# https://docs.python.org/3/py-modindex.html
+
+from sys import platform
+
+from random import randint
+
+def randint(*args):
+    return 'hahaha'
+
+for i in range(10):
+    print(randint(0,10))
